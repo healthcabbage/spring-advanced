1252d1ad4fd8631f5bcd49c573749da16c41d35e
Cabbagggggggge <gpwjd7332@naver.com>
2026-09-11 01:15:43 +0900

    [Feat] 진행과 전체 덱 저장 기능
    주석 해제하고 코드 추가 생성

--- src/main/java/com/gamebasic/game/controller/GameController.java
+++ src/main/java/com/gamebasic/game/controller/GameController.java
@@ -2,6 +2,7 @@ package com.gamebasic.game.controller;
 
 import com.gamebasic.game.dto.CreateRequest;
 import com.gamebasic.game.dto.GameDetailResponse;
+import com.gamebasic.game.dto.ProgressRequest;
 import com.gamebasic.game.service.GameService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,8 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 
@@ -33,11 +37,11 @@ public class GameController {
     }
 
     // TODO (Lv 6): 진행과 전체 덱 저장. 주석을 풀고 구현하세요.
-    // @PutMapping("/games/{gameId}/progress")
-    // public ResponseEntity<?> updateProgress(
-    //     @PathVariable Long gameId,
-    //     @Valid @RequestBody ProgressRequest request
-    // ) {
-    //     return ResponseEntity.ok(gameService.updateProgress(gameId, request));
-    // }
+     @PutMapping("/games/{gameId}/progress")
+     public ResponseEntity<?> updateProgress(
+         @PathVariable Long gameId,
+         @Valid @RequestBody ProgressRequest request
+     ) {
+         return ResponseEntity.ok(gameService.updateProgress(gameId, request));
+     }
 }
